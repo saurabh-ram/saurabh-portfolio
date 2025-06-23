4d20428dded57fc5a6194a2828bda9d0214a49da
Saurabh Ram <saurabhram37@gmail.com>
2025-06-23 12:29:46 +0530

    Update script-1.js

--- scripts/script-1.js
+++ scripts/script-1.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let animation2Completed = false;
   let animation5Completed = false;
   let currentImage = 1;
-  const totalNoOfImages = 27;
+  const totalNoOfImages = 28;
 
   // Trigger animation-1 on clicking image-1
   if (greenbutton && omnitrixbutton) {
